367602a6e771d3cc69a972ceaaf842ce98ec95a4
Rhaisa <rhaisa@Rhaisas-MacBook-Air.local>
2023-01-29 20:37:12 -0300

    Second commit

---
+++ idadeDeCachorro/idadeDeCachorro/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  idadeDeCachorro
+//
+//  Created by Rhaisa on 18/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro é: " + String(idade)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
